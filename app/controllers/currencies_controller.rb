424b702021746d5faae40d6f99888255838a920b
etrinklein <etrinklein@gmail.com>
2021-04-26 21:10:18 +0000

    all but the conversion

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -21,4 +21,15 @@ class CurrenciesController < ApplicationController
 
     render({ :template => "currency_templates/step_two.html.erb"})
   end
+
+  def conversion
+    @from_symbol = params.fetch("from")
+    @to_symbol = params.fetch("to")
+    
+    @rawdata = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@rawdata)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    render({ :template => "currency_templates/step_three.html.erb"})
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   get("/forex",{ :controller => "currencies", :action => "first_currency"})
   get("/forex/:currency",{ :controller => "currencies", :action => "second_currency"})
+  get("/forex/:from/:to",{ :controller => "currencies", :action => "conversion"})
+
 end
